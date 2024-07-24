d63ac7237ed9c887d632ec3d37f466bc1d1a54c3
emu3011 <149319493+emu3011@users.noreply.github.com>
2024-07-25 01:43:59 +0200

    Update RunIdModifier.java
    Add Author comment.

--- contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/parkingGuidanceSystem/RunIdModifier.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/parkingGuidanceSystem/RunIdModifier.java
@@ -1,5 +1,10 @@
 package org.matsim.contrib.parking.parkingsearch.manager.parkingGuidanceSystem;
 
+/**
+ * Runs the IdModifier with values specified in the PGSConfigurator.
+ * 
+ * @author Emanuel Skodinis (emanuesk@ethz.ch)
+ */
 public class RunIdModifier {
     public static void main(String[] args) {
 		IdModifier.modifyPersonIdsToStrategy(PGSConfigurator.sourcePopulationFilePath,
