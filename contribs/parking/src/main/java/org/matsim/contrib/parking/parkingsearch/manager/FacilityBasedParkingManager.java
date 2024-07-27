5853957f07cb776702be212898527ab6b716d54e
emu3011 <149319493+emu3011@users.noreply.github.com>
2024-07-27 02:18:31 +0200

    Update FacilityBasedParkingManager.java
    Remove bug: initial number of free parking spots is capacity and not 0.

--- contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/FacilityBasedParkingManager.java
+++ contribs/parking/src/main/java/org/matsim/contrib/parking/parkingsearch/manager/FacilityBasedParkingManager.java
@@ -113,7 +113,7 @@ public class FacilityBasedParkingManager implements ParkingSearchManager, Shutdo
 
 			// added by Emanuel Skodinis (emanuesk@ethz.ch): write initial occupation of the facilities
 			int capacity = PGSUtils.getCapacity(fac);
-			String[] row = {String.valueOf(fac.getCoord().getX()), String.valueOf(fac.getCoord().getY()), fac.getId().toString(), "25200.0", String.valueOf(capacity), "0", "0", "0.0"};
+			String[] row = {String.valueOf(fac.getCoord().getX()), String.valueOf(fac.getCoord().getY()), fac.getId().toString(), "25200.0", String.valueOf(capacity), "0", String.valueOf(capacity), "0.0"};
 			writer.writeNext(row);
 
 			this.reservationsRequests.put(fac.getId(), new MutableLong(0));
